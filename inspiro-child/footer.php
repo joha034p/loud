768c6c998dbd8b53c77c2f447141de44a20029c0
Lasse Smedegaard <Smedegaard@Lasses-MBP.lan>
2021-04-29 18:00:37 +0200

    footer

--- inspiro-child/footer.php
+++ inspiro-child/footer.php
@@ -19,11 +19,64 @@
 		<?php get_template_part( 'template-parts/footer/footer', 'instagram-widget' ); ?>
 
 		<footer id="colophon" <?php inspiro_footer_class(); ?> role="contentinfo">
-			<div class="inner-wrap">
-				<?php
-				get_template_part( 'template-parts/footer/footer', 'widgets' );
+			<div class="inner-wrap footer-wrapper">
+				<div>
+					<div class="column-header">
+						Hent appen
+					</div>
+					<ul>
+						<li>
+							<a href="https://apps.apple.com/dk/app/radio-loud/id1498746367">
+								<img src="<?php echo get_stylesheet_directory_uri() ?>/assets/app-store.svg" />
+							</a>
+						</li>
+						<li>
+							<a href="https://play.google.com/store/apps/details?id=dk.uptime.RadioLoud&hl=da">
+								<img src="<?php echo get_stylesheet_directory_uri() ?>/assets/google-play.png" />
+							</a>
+						</li>
+					</ul>
+				</div>
+				<div>
+					<div class="column-header">
+						Indhold
+					</div>
+					<ul>
+						<li><a href="/loud/podcasts">Podcasts</a></li>
+						<li></li>
+					</ul>
+				</div>
+				<div>
+					<div class="column-header">
+						Om Radio Loud
+					</div>
+					<ul>
+						<li><a href="/loud/loud-live">LOUD LIVE</a></li>
+						<li><a href="/loud/om-radio-loud/">Hvem er vi</a></li>
 
-				get_template_part( 'template-parts/footer/site', 'info' );
+					</ul>
+				</div>
+				<div>
+					<div class="column-header">
+						Kontaktinformation
+					</div>
+					<ul>
+						<li><a href="mailto:kontakt@radioloud.dk">kontakt@radioloud.dk</a></li>
+						<li><a href="tel:+4532421714">+45 32 42 17 14</a></li>
+						<li>PROGRAMREDAKTION</li>
+						<li> <div class="smuktekst"> Wildersgade 10B, 2. sal </div> </li>
+						<li> <div class="smuktekst"> 1408 København </div></li>
+						<li>NYHEDSREDAKTION</li>
+						<li> <div class="smuktekst"> Vestergade 165D, 2. sal</div></li>
+						<li> <div class="smuktekst"> 5700 Svendborg</div></li>
+
+
+
+					</ul>
+				</div>
+				 <?php
+				// get_template_part( 'template-parts/footer/footer', 'widgets' );
+				// get_template_part( 'template-parts/footer/site', 'info' );
 				?>
 			</div><!-- .inner-wrap -->
 		</footer><!-- #colophon -->
@@ -32,4 +85,39 @@
 <?php wp_footer(); ?>
 
 </body>
+
+<style>
+.footer-wrapper {
+	padding: 20px 0;
+	width: 80%;
+    margin: 0 auto;
+	display: flex;
+}
+.footer-wrapper div {
+	flex-basis: 25%;
+}
+.footer-wrapper .column-header {
+	text-transform: uppercase;
+}
+.footer-wrapper ul {
+	margin-left: 0;
+	list-style: none;
+}
+.footer-wrapper ul li {
+	margin: 7px 0;
+	
+}
+.footer-wrapper ul a {
+	color: white;
+}
+.footer-wrapper ul img {
+	width: 128px;
+}
+
+.smuktekst {
+	color: white;
+}
+
+</style>
+
 </html>
