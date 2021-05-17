c39523dbd65c4c2c739e0c06e4abc582898dde73
Johan Santaolalla Rives Larsen <johanrives@Johans-MacBook-Pro.local>
2021-05-17 12:27:43 +0200

    al kode kopieret fra loud

---
+++ inspiro-child-morrow/page-podcasts.php
@@ -0,0 +1,108 @@
+<?php
+
+//The template for displaying podcasts
+
+
+get_header();
+?>
+
+
+<section id="primary" class="content-area">
+    <main id="main" class="site-main inner-wrap">
+        <div class="titelWrap">
+            <h2 class="podcastTitel">Podcasts</h2>
+            <p>Find vores mange programmer, udsendelser, serier, afsnit og episoder her.</p>
+        </div>
+        <nav id="filtrering"><button data-podcast="alle">Alle</button></nav>
+        <section class="podcastcontainer"></section>
+
+        <template>
+            <div class="article_container">
+                <img class="podcast_image" src="" alt="">
+                <div class="txt_container">
+                    <h2 class="podcastTitles"></h2>
+                    <p class="kortbeskrivelse"></p>
+                    <p class="antal_episoder"><b>Episoder:</b> </p>
+                </div>
+            </div>
+        </template>
+    </main>
+
+    <script>
+        let podcasts;
+        let categories;
+        let episoder;
+        let filterPodcast = "alle";
+        const url = "http://johanrives.dk/loud/wp-json/wp/v2/podcast?per_page=100";
+        const catUrl = "http://johanrives.dk/loud/wp-json/wp/v2/categories";
+        const episodeUrl = "http://johanrives.dk/loud/wp-json/wp/v2/episode?per_page=100";
+
+        async function getJson() {
+            const data = await fetch(url);
+            const catData = await fetch(catUrl);
+            podcasts = await data.json();
+            categories = await catData.json();
+            console.log(categories);
+
+            const data2 = await fetch(episodeUrl);
+            episoder = await data2.json();
+            console.log("episoder: ", episoder);
+
+            visPodcasts();
+            opretKnapper();
+        }
+
+        function opretKnapper() {
+            categories.forEach(cat => {
+                document.querySelector("#filtrering").innerHTML += `<button class="filter" data-podcast="${cat.id}">${cat.name}</button>`
+            })
+
+            addEventListenersToButtons();
+        }
+
+        function addEventListenersToButtons() {
+            document.querySelectorAll("#filtrering button").forEach(elm => {
+                elm.addEventListener("click", filtrering);
+            })
+        }
+
+        function filtrering() {
+            filterPodcast = this.dataset.podcast;
+            console.log(filterPodcast);
+            visPodcasts();
+        }
+
+        function visPodcasts() {
+            let antalEpisoder = 0;
+            console.log(podcasts);
+            let temp = document.querySelector("template");
+            let container = document.querySelector(".podcastcontainer");
+            container.innerHTML = "";
+            podcasts.forEach(podcast => {
+                if (filterPodcast == "alle" || podcast.categories.includes(parseInt(filterPodcast))) {
+                    let klon = temp.cloneNode(true).content;
+                    if (podcast.episoder.length > 0) {
+                        klon.querySelector(".antal_episoder").textContent += podcast.episoder.length;
+                    } else {
+                        klon.querySelector(".antal_episoder").textContent += 0;
+                    }
+                    klon.querySelector("h2").textContent = podcast.title.rendered;
+                    klon.querySelector("img").src = podcast.billede.guid;
+                    klon.querySelector(".kortbeskrivelse").textContent = podcast.kortbeskrivelse;
+                    klon.querySelector(".article_container").addEventListener("click", () => {
+                        location.href = podcast.link;
+                    })
+                    container.appendChild(klon);
+                }
+            })
+        }
+
+        getJson();
+
+    </script>
+</section>
+
+
+
+<?php
+get_footer();
---
+++ inspiro-child-morrow/single-podcast.php
@@ -0,0 +1,109 @@
+<?php
+
+//The template for displaying single podcast
+
+
+get_header();
+?>
+
+
+<section id="primary" class="content-area">
+    <main id="main" class="site-main">
+        <article>
+            <div class="podContainer">
+                <img class="pic" src="" alt="">
+                <div>
+                    <h2></h2>
+                    <p class="langbeskrivelse"></p>
+                    <p class="antal_episoder"></p>
+                </div>
+            </div>
+        </article>
+        <h2 class="episodertxt">Episoder</h2>
+
+        <section id="episoder">
+            <template>
+                <article>
+                    <div class="episoder_article_container">
+                        <img class="episodePic" src="" alt="">
+                        <div class="episoder_txt_container">
+                            <h2 class="episodeOverskrift"></h2>
+                            <p class="beskrivelse"></p>
+                            <p class="varighed"></p>
+                            <p class="udgivelsesdato"></p>
+                            <a href="">Læs mere</a>
+                        </div>
+                    </div>
+                </article>
+            </template>
+        </section>
+    </main>
+
+    <script>
+        let podcast;
+        let episoder;
+        let aktuelpodcast = <?php echo get_the_ID() ?>;
+        const url = "http://johanrives.dk/loud/wp-json/wp/v2/podcast/" + aktuelpodcast;
+        const episodeUrl = "http://johanrives.dk/loud/wp-json/wp/v2/episode?per_page=100";
+
+        const container = document.querySelector("#episoder");
+
+        async function getJson() {
+            const data = await fetch(url);
+            podcast = await data.json();
+
+            const data2 = await fetch(episodeUrl);
+            episoder = await data2.json();
+            console.log("episoder: ", episoder);
+
+            visPodcasts();
+            visEpisoder();
+        }
+
+        function visPodcasts() {
+            let antalEpisoder = 0;
+            document.querySelector("h2").textContent = podcast.title.rendered;
+            document.querySelector(".pic").src = podcast.billede.guid;
+            document.querySelector(".langbeskrivelse").textContent = podcast.langbeskrivelse;
+
+            episoder.forEach(episode => {
+                console.log("loop id :", aktuelpodcast);
+                if (episode.episode_til_podcasten == aktuelpodcast) {
+                    antalEpisoder++
+                }
+            })
+            document.querySelector(".antal_episoder").textContent = "Episoder: " + antalEpisoder;
+        }
+
+
+        function visEpisoder() {
+            let temp = document.querySelector("template");
+
+            episoder.forEach(episode => {
+                console.log("loop id :", aktuelpodcast);
+                if (episode.episode_til_podcasten == aktuelpodcast) {
+
+                    let klon = temp.cloneNode(true).content;
+                    klon.querySelector(".episodePic").src = episode.billede.guid;
+                    klon.querySelector(".episodeOverskrift").textContent = episode.title.rendered;
+                    klon.querySelector(".varighed").innerHTML = episode.varighed;
+                    klon.querySelector(".udgivelsesdato").innerHTML = episode.udgivelsesdato;
+                    klon.querySelector("article").addEventListener("click", () => {
+                        location.href = episode.link;
+                    })
+
+                    klon.querySelector("a").href = episode.link;
+                    console.log("episode", episode.link);
+                    container.appendChild(klon);
+                }
+            })
+        }
+
+        getJson();
+
+    </script>
+</section>
+<!--#primary-->
+
+<?php
+get_footer();
